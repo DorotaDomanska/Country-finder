16137e98fca176b9a90ffa44cc23049a71783698
DorotaDomanska <dorota@domanska.eu>
2022-12-04 14:56:19 +0100

    update

--- src/index.js
+++ src/index.js
@@ -5,30 +5,34 @@ import debounce from "lodash.debounce";
 const DEBOUNCE_DELAY = 300;
 const input = document.querySelector("input#search-box");
 const countryList = document.querySelector("ul.country-list");
+const countryInfo = document.querySelector("div.country-info");
 
 const fetchCountries = (name) => {
   const trimedName = name.trim();
 
-  return (
-    fetch(
-      `https://restcountries.com/v2/name/${trimedName}?fields=name,capital,population,flags,languages`
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return console.log(response.json());
-      })
-      // .then((countries) => {
-      //   createCountryList(countries);
-      // })
-      .catch((error) => console.error(error))
-  );
+  return fetch(
+    `https://restcountries.com/v2/name/${trimedName}?fields=name,capital,population,flags,languages`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then((countries) => {
+      if (countries.length > 10)
+        return Notiflix.Notify.info(
+          `Too many matches found. Please enter a more specific name.`
+        );
+      if (countries.length === 1) return createCountryCard(countries[0]);
+      return createCountryList(countries);
+    })
+    .catch((error) => console.error(error));
 };
 
 const createCountryCard = ({ name, capital, population, flags, languages }) => {
   const allLanguages = languages.map((language) => language.name).join(", ");
-  const country = document.createElement("li");
+  const country = document.createElement("p");
   country.innerHTML = `
   <h3>
     <img src="${flags.svg}" alt="${name} flag" width="50px" />
@@ -37,7 +41,7 @@ const createCountryCard = ({ name, capital, population, flags, languages }) => {
   <p>Capital: ${capital}</p>
   <p>Population: ${population}</p>
   <p>Languages: ${allLanguages}</p>`;
-  countryList.append(country);
+  countryInfo.append(country);
 };
 
 const createCountryList = (countries) => {
