b7fc42a5cf64a1b55e844ab3c815913b63511f2c
DorotaDomanska <dorota@domanska.eu>
2022-12-04 13:01:15 +0100

    update

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import debounce from "lodash.debounce";
 
 const DEBOUNCE_DELAY = 300;
 const input = document.querySelector("input#search-box");
+const countryList = document.querySelector("ul.country-list");
 
 const fetchCountries = (name) => {
-  console.log(name);
   return fetch(
     `https://restcountries.com/v3/name/${name}?fields=name,capital,population,flags,languages`
   ).then((response) => {
@@ -17,11 +17,20 @@ const fetchCountries = (name) => {
   });
 };
 
-// const countriesList = () => {
-
-// }
+const createCountryList = ({ name, capital, population, flags, languages }) => {
+  const country = document.createElement("li");
+  country.innerHTML = `
+  <h3>
+    <img src="${flags.svg}" alt="${name.official} flag" width="50px" />
+    ${name.official}
+  </h3>
+   <p>Capital: ${capital}</p>
+  <p>Population: ${population}</p>
+  <p>Languages: ${languages}</p>`;
+  countryList.append(country);
+};
 
 input.addEventListener(
   "input",
-  debounce((event) => fetchCountries(event.target.value), 300)
+  debounce((event) => fetchCountries(event.target.value), DEBOUNCE_DELAY)
 );
