78c3053c80232e59879f42338e1292736bc31b87
DorotaDomanska <dorota@domanska.eu>
2022-12-04 14:30:28 +0100

    update

--- src/index.js
+++ src/index.js
@@ -7,27 +7,49 @@ const input = document.querySelector("input#search-box");
 const countryList = document.querySelector("ul.country-list");
 
 const fetchCountries = (name) => {
-  return fetch(
-    `https://restcountries.com/v3/name/${name}?fields=name,capital,population,flags,languages`
-  ).then((response) => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return console.log(response.json());
-  });
+  const trimedName = name.trim();
+
+  return (
+    fetch(
+      `https://restcountries.com/v2/name/${trimedName}?fields=name,capital,population,flags,languages`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return console.log(response.json());
+      })
+      // .then((countries) => {
+      //   createCountryList(countries);
+      // })
+      .catch((error) => console.error(error))
+  );
 };
 
-const createCountryList = ({ name, capital, population, flags, languages }) => {
+const createCountryCard = ({ name, capital, population, flags, languages }) => {
+  const allLanguages = languages.map((language) => language.name).join(", ");
   const country = document.createElement("li");
   country.innerHTML = `
   <h3>
-    <img src="${flags.svg}" alt="${name.official} flag" width="50px" />
-    ${name.official}
+    <img src="${flags.svg}" alt="${name} flag" width="50px" />
+    ${name}
   </h3>
-   <p>Capital: ${capital}</p>
+  <p>Capital: ${capital}</p>
   <p>Population: ${population}</p>
-  <p>Languages: ${languages}</p>`;
+  <p>Languages: ${allLanguages}</p>`;
   countryList.append(country);
+};
+
+const createCountryList = (countries) => {
+  const allCountries = countries.map(({ flags, name }) => {
+    const item = document.createElement("li");
+    item.innerHTML = `
+    <img src="${flags.svg}" alt="${name} flag" width="50px" /> ${name}`;
+
+    return item;
+  });
+
+  countryList.append(...allCountries);
 };
 
 input.addEventListener(
