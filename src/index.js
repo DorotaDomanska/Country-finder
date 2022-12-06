c059d222b669d18cbbf523dbfefee61265c706f2
DorotaDomanska <dorota@domanska.eu>
2022-12-06 23:32:39 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./css/styles.css";
+import fetchCountries from "./fetchCountries";
 import Notiflix from "notiflix";
 import debounce from "lodash.debounce";
 
@@ -6,40 +7,6 @@ const DEBOUNCE_DELAY = 300;
 const input = document.querySelector("input#search-box");
 const countryList = document.querySelector("ul.country-list");
 const countryInfo = document.querySelector("div.country-info");
-
-const fetchCountries = (name) => {
-  if (name === "") {
-    countryList.innerHTML = "";
-    countryInfo.innerHTML = "";
-    return;
-  }
-  const trimedName = name.trim();
-
-  return fetch(
-    `https://restcountries.com/v2/name/${trimedName}?fields=name,capital,population,flags,languages`
-  )
-    .then((response) => {
-      if (!response.ok) {
-        return Notiflix.Notify.failure(
-          `Oops, there is no country with that name`
-        );
-      }
-      return response.json();
-    })
-    .then((countries) => {
-      if (countries.length > 10)
-        return Notiflix.Notify.info(
-          `Too many matches found. Please enter a more specific name.`
-        );
-      if (countries.length === 1) {
-        if (!(`capital` in countries))
-          return createCountryWithNoCapital(countries[0]);
-        return createCountryCard(countries[0]);
-      }
-      return createCountryList(countries);
-    })
-    .catch((error) => console.error(error));
-};
 
 const createCountryCard = ({ name, capital, population, flags, languages }) => {
   const allLanguages = languages.map((language) => language.name).join(", ");
@@ -60,8 +27,8 @@ const createCountryCard = ({ name, capital, population, flags, languages }) => {
 
 const createCountryWithNoCapital = ({ name, population, flags, languages }) => {
   const allLanguages = languages.map((language) => language.name).join(", ");
-  const country = document.createElement("article");
-  country.innerHTML = `
+  const countryNoCapital = document.createElement("article");
+  countryNoCapital.innerHTML = `
   <h3>
     <img src="${flags.svg}" alt="${name} flag" width="50px" />
     ${name}
@@ -72,7 +39,7 @@ const createCountryWithNoCapital = ({ name, population, flags, languages }) => {
 
   countryList.innerHTML = "";
   countryInfo.innerHTML = "";
-  countryInfo.append(country);
+  countryInfo.append(countryNoCapital);
 };
 
 const createCountryList = (countries) => {
@@ -102,3 +69,11 @@ input.addEventListener(
 function onlyLettersAndSpacesAndDashes(str) {
   return /^[A-Za-z\s\-]*$/.test(str);
 }
+
+export {
+  countryInfo,
+  countryList,
+  createCountryList,
+  createCountryCard,
+  createCountryWithNoCapital,
+};
